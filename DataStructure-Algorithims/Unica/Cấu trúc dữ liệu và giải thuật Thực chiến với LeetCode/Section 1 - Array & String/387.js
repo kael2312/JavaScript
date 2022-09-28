3dca9be37fcbcd9d1769bd468d369923fb50f5f4
kael2312 <lenhathoang2311@gmail.com>
2022-09-28 09:24:13 +0700

    commit leetcode 387

---
+++ DataStructure-Algorithims/Unica/Cấu trúc dữ liệu và giải thuật Thực chiến với LeetCode/Section 1 - Array & String/387.js
@@ -0,0 +1,28 @@
+/*
+ *387. First Unique Character in a String
+ *Given a string s, find the first non-repeating character in it and return its index.
+ *If it does not exist, return -1.
+
+*Example 1:
+*Input: s = "leetcode"
+*Output: 0
+ */
+
+var firstUniqChar = function (s) {
+    let arrCharacter = [...s];
+    let char;
+    for (let i = 0; i < arrCharacter.length; i++) {
+        if (
+            s.indexOf(arrCharacter[i]) == i &&
+            s.indexOf(arrCharacter[i], i + 1) === -1
+        ) {
+            char = arrCharacter[i];
+            break;
+        }
+    }
+    let index = arrCharacter.findIndex((x) => x === char);
+    return index;
+};
+
+let s = 'aabb';
+console.log(firstUniqChar(s));
