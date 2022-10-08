7471c40de4f0952c030b724e89494083c78c4dc5
kael2312 <lenhathoang2311@gmail.com>
2022-10-08 17:32:05 +0700

    commit recursion theory

--- DataStructure-Algorithims/Unica/Cấu trúc dữ liệu và giải thuật Thực chiến với LeetCode/Section 4 - Recursion/index.js
+++ DataStructure-Algorithims/Unica/Cấu trúc dữ liệu và giải thuật Thực chiến với LeetCode/Section 4 - Recursion/index.js
@@ -10,12 +10,68 @@ function fibonacciCalculator(index) {
     return fibonacciCalculator(index - 1) + fibonacciCalculator(index - 2);
 }
 
-function factorialPrintf(array) {
-    let count = array.length - 1;
-    if (count <= 0) return;
-    console.log(array[count]);
+let F = [];
+function refactorFibonacci(index) {
+    if (F[index] != undefined) return F[index];
+
+    if (index <= 2) {
+        F[1] = 1;
+        F[2] = 1;
+        return 1;
+    }
+
+    F[index] = refactorFibonacci(index - 1) + refactorFibonacci(index - 2);
+    return F[index];
+}
+
+function forLoopFibonacci(index) {
+    let F = [];
+    F[1] = 1;
+    F[2] = 1;
+    for (let i = 0; i <= index; i++) {
+        F[i] = F[i - 1] + f[i - 2];
+    }
+    return F[index];
+}
+
+function recursionPrintf(array, index) {
+    if (index > array.length - 1 || index < 0) return;
+    recursionPrintf(array, index + 1);
+    console.log(array[index]);
+}
+
+function revertRecursionPrintf(array, index) {
+    if (index > array.length - 1 || index < 0) return;
+    recursionPrintf(array, index + 1);
+    console.log(array[index]);
+}
+
+let max = -1;
+function findMaxNonReturnValue(array, index) {
+    if (index < array.length) {
+        if (array[index] > max) {
+            max = array[index];
+        }
+        findMaxNonReturnValue(array, index + 1);
+    }
+}
+
+function findMaxReturnValue(array, index, previousMax) {
+    if (index < array.length) {
+        let currentMax = Math.max(previousMax, array[index]);
+        return findMaxReturnValue(array, index + 1, currentMax);
+    }
+    return previousMax;
 }
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+let arrNumber = [1, 2, 3];
 console.log('Factorial of 5: ', factorialCalculator(5));
-console.log('5th Fibonacci number is: ', fibonacciCalculator(5));
+console.log('5th Fibonacci number is: ', fibonacciCalculator(10));
+console.log('5th Fibonacci number using refactor is: ', refactorFibonacci(10));
+console.log('5th Fibonacci number using for loop is: ', refactorFibonacci(10));
+console.log(recursionPrintf(arr, 5));
+console.log(revertRecursionPrintf(arr, 5));
+findMaxNonReturnValue(arr, 0);
+console.log('Max: ', max);
+console.log(findMaxReturnValue(arrNumber, 0, -1));
