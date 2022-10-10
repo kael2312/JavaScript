8b3775e5b445c99b550a9f427c63bdcc8227eb87
kael2312 <lenhathoang2311@gmail.com>
2022-10-10 10:30:10 +0700

    commit leetcode 1346

--- DataStructure-Algorithims/Unica/Cấu trúc dữ liệu và giải thuật Thực chiến với LeetCode/Section 2 - Array & String/1346.js
+++ DataStructure-Algorithims/Unica/Cấu trúc dữ liệu và giải thuật Thực chiến với LeetCode/Section 2 - Array & String/1346.js
@@ -1,10 +1,14 @@
 var checkIfExist = function (arr) {
-    let j = 0;
+    const obj = {};
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] != arr[i + 1]) {
-            j++;
-        } else {
-            return false;
-        }
+        let halfVal = arr[i] / 2;
+        let doubleVal = arr[i] * 2;
+        if (obj[doubleVal]) return true;
+        if (obj[halfVal]) return true;
+        obj[arr[i]] = 1;
     }
+    return false;
 };
+
+arrayNUmber = [7, 1, 14, 11];
+console.log(checkIfExist(arrayNUmber));
