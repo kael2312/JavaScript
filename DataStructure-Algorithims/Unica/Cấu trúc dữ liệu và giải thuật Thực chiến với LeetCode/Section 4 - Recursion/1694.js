9b04217ffeea6f858a9647acd773990ab5fdfa39
kael2312 <lenhathoang2311@gmail.com>
2022-10-11 22:01:41 +0700

    commit leetcode 1694

--- DataStructure-Algorithims/Unica/Cấu trúc dữ liệu và giải thuật Thực chiến với LeetCode/Section 4 - Recursion/1694.js
+++ DataStructure-Algorithims/Unica/Cấu trúc dữ liệu và giải thuật Thực chiến với LeetCode/Section 4 - Recursion/1694.js
@@ -1,1 +1,12 @@
-var reformatNumber = function (number) {};
+var reformatNumber = function (number) {
+    return Noname(number.replaceAll(/[ -]/g, ''));
+};
+
+function Noname(str) {
+    if (str.length == 2 || str.length == 3) return str;
+    if (str.length == 4) return str.substring(0, 2) + '-' + str.substring(2, 4);
+    return str.substring(0, 3) + '-' + Noname(str.substring(3, str.length));
+}
+
+let string = '1-23-45 6';
+console.log(reformatNumber(string));
