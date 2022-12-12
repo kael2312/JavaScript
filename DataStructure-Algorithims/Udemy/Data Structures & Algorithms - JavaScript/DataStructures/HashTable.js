1e7d004d9b8d7ab57f4651b95fb1df204bb68c7b
hoangln1 <lenhathoang2311@gmail.com>
2022-12-12 16:53:49 +0700

    commit hashtable

--- DataStructure-Algorithims/Udemy/Data Structures & Algorithms - JavaScript/DataStructures/HashTable.js
+++ DataStructure-Algorithims/Udemy/Data Structures & Algorithms - JavaScript/DataStructures/HashTable.js
@@ -12,4 +12,28 @@ class HashTable {
         return hash;
         0;
     }
+
+    set(key, value) {
+        let index = this._hash(key);
+        if (!this.dataMap[index]) {
+            this.dataMap[index] = [];
+        }
+        this.dataMap[index].push([key, value]);
+    }
+
+    get(key) {
+        let index = this._hash(key);
+        if (this.dataMap[index]) {
+            for (let i = 0; i < this.dataMap[index].length; i++) {
+                if (this.dataMap[index][i][0] === key) {
+                    return this.dataMap[index][i][1];
+                }
+            }
+        }
+        return undefined;
+    }
 }
+
+let myHasTable = new HashTable();
+myHasTable.set('bolts', 1500);
+myHasTable.set('washers', 50);
