c4ddd2b6b1dc5784360ad972b1e08376eef38433
hoangln1 <lenhathoang2311@gmail.com>
2022-12-22 16:36:27 +0700

    commit graph udemy

---
+++ DataStructure-Algorithims/Udemy/Data Structures & Algorithms - JavaScript/Algorithms/BubbleSort.js
@@ -0,0 +1,14 @@
+function BubbleSort(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - 1 - i; j++) {
+            if (array[j] > array[j + 1]) {
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+console.log('Bubble Sort: ', BubbleSort([4, 2, 6, 5, 1, 3]));
