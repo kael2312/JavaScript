a4293df55a98afdd0536d35c1c5b403451e1a77f
hoangln1 <lenhathoang2311@gmail.com>
2022-12-28 22:21:06 +0700

    commit leetcode 1636

---
+++ DataStructure-Algorithims/Unica/Cấu trúc dữ liệu và giải thuật Thực chiến với LeetCode/Section 2 - Array & String/1636.js
@@ -0,0 +1,15 @@
+var frequencySort = function (nums) {
+    const map = new Map();
+    for (let n of nums) {
+        map.set(n, (map.get(n) || 0) + 1);
+    }
+    return nums.sort((a, b) => {
+        if (map.get(a) === map.get(b)) {
+            return b - a;
+        } else {
+            return map.get(a) - map.get(b);
+        }
+    });
+};
+
+console.log('Leetcode 1636: ', frequencySort([1, 1, 2, 2, 3]));
